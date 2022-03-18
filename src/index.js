3c3d34a058276f5d1392087324fedeb43b9f1a33
Alice Liu <aliceliu@jlsemi.com>
2022-03-17 20:18:47 -0400

    add the contactus, login, signup component design

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import './css/index.css'
+import './css/css/all.css'
+import Contactus from './pages/Contactus'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+// ReactDOM.render(<Contactus />, document.querySelector("#root"));
+// ReactDOM.render(<Login />, document.querySelector("#root"));
+ReactDOM.render(<Signup />, document.querySelector("#root"));
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Signup() {
+  return (
+      <div className="signup">
+          <h3>Sign Up</h3>
+          <form action="" className='signup-form'>
+          <label htmlFor="fullname">Full Name</label><br />
+            <input type="text" name="fullname" id="fullname" placeholder='Full Name' />
+            <br />
+            <label htmlFor="signup-">Email</label><br />
+            <input type="email" name="signup-email" id="signup-email" placeholder='e-mail address' />
+            <br />
+            <label htmlFor="password">Password</label>
+            <br />
+            <input type="password" name='password' id="password" placeholder='Password' />
+           <br />
+           <label htmlFor="cfm-password">Confirm Password</label>
+            <br />
+            <input type="password" name='cfm-password' id="cfm-password" placeholder='Confirm Password' />
+           <br />
+            <input type="checkbox" name='terms' id="terms" value="term-agree" /><label htmlFor="terms">I agree the <a href=""><strong>Terms and Conditions</strong></a></label>
+            <br />
+            <input type="submit" value="Sign Up" id= "signup-btn"/>
+            <p>New to Food Delivery System? <a href="">Sign up</a></p>
+          </form>
+      </div>
+    
+  )
+}
