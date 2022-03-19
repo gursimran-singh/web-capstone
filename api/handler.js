5b2251491c4233da9e18ce79d04b19ea21c5faff
Snehal2207 <snehalmotisariya@gmail.com>
2022-03-18 20:48:46 -0400

    replace AWS code in handler.js

--- api/handler.js
+++ api/handler.js
@@ -2,6 +2,13 @@ const app = require("./src/index");
 require("dotenv").config();
 
 if (process.env.MODE == "local") {
+  //**********************aws*************************************
+  var AWS = require("aws-sdk");
+  AWS.config.update({
+    region: "ca-central-1", // replace with your region in AWS account
+  });
+  var credentials = new AWS.SharedIniFileCredentials({ profile: "capstone" });
+  AWS.config.credentials = credentials;
   app.listen(process.env.PORT, () =>
     console.log(`Example app listening on port ${process.env.PORT}!`)
   );
--- api/src/index.js
+++ api/src/index.js
@@ -2,14 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-//**********************aws*************************************
-var AWS = require("aws-sdk");
-AWS.config.update({
-  region: "ca-central-1", // replace with your region in AWS account
-});
-var credentials = new AWS.SharedIniFileCredentials({ profile: "capstone" });
-AWS.config.credentials = credentials;
-
 var apiRouter = require("./routes/indexRouteAPI");
 
 const app = express();
