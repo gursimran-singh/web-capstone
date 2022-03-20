45f5abfed0bef997a24ebc1a4c2b433ae2a8a364
Alice Liu <aliceliu@jlsemi.com>
2022-03-20 17:50:18 -0400

    add

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom';
+import './css/index.css'
+import './css/css/fontawesome.css'
+import './css/css/brands.css'
+import './css/css/solid.css'
+import Contactus from './pages/Contactus'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
+// ReactDOM.render(<Contactus />, document.querySelector("#root"));
+// ReactDOM.render(<Login />, document.querySelector("#root"));
+// ReactDOM.render(<Signup />, document.querySelector("#root"));
+ReactDOM.render(<Cart />, document.querySelector("#root"));
+// ReactDOM.render(<Checkout />, document.querySelector("#root"))
