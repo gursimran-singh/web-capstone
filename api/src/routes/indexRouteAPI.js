38d62aa296517584ca5a81dfe30e4fb0fbccec33
Snehal2207 <snehalmotisariya@gmail.com>
2022-03-23 17:56:36 -0400

    Updated post/put

--- api/src/routes/indexRouteAPI.js
+++ api/src/routes/indexRouteAPI.js
@@ -2,15 +2,17 @@ const express = require("express");
 const router = express.Router();
 const itemCtrl = require("../controllers/itemController");
 
-router.route("/food")
+router.route("/")
     .get(itemCtrl.getAllItems)
     .post(itemCtrl.postOneItem);
 
-router.route("/food/:foodid").get(itemCtrl.getOneItem)
+router.route("/:foodid")
+    .get(itemCtrl.getOneItem)
     .put(itemCtrl.putOneItem)
-    // .delete(itemCtrl.deleteOneItem1);
+    .delete(itemCtrl.deleteItemById);
 
-router.route("/food/delete/:foodid")
+router.route("/delete/:foodid")
     .put(itemCtrl.deleteOneItem);
+
 
 module.exports = router;
