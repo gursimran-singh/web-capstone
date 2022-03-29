fb7d2c582b398f2f803870cb76ce416c03165ebb
Alice Liu <aliceliu@jlsemi.com>
2022-03-29 00:28:28 -0400

    Update the login and signup and authSlice

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -8,14 +8,14 @@ import { login } from "./slices/authSlice";
 import { clearMessage } from "./slices/messageSlice";
 import {Link, Outlet} from "react-router-dom"
 
+import "bootstrap/dist/css/bootstrap.css";
 import './contactus.css'
 
-export default function Login(props) {
-  const [loading, setLoading] = useState(false);
-
-  const { isLoggedIn } = useSelector((state) => state.auth);
-  const { message } = useSelector((state) => state.message);
-
+export default function Login() {
+  const [status, setStatus] = useState(false);
+  const  message  = useSelector((state) => state.message.message);
+  // const token =useSelector((state) => state.auth.token);
+  // console.log(token);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -28,26 +28,32 @@ export default function Login(props) {
   };
 
   const validationSchema = Yup.object().shape({
-    email: Yup.string().required("Email is required!"),
-    password: Yup.string().required("Password is required!"),
+    email: Yup.string()
+    .email("This is not a valid email.")
+    .required("Email is required!"),
+    password: Yup.string().test(
+      "len",
+      "The password must be between 6 and 30 characters.",
+      (val) =>
+        val && val.toString().length >= 6 && val.toString().length <= 30
+    ).required("Password is required!"),
   });
 
-  const handleLogin = (formValue) => {
-    const { email, password } = formValue;
-    setLoading(true);
+  const handleLogin = (submitVal) => {
+    const { email, password } = submitVal;
+    setStatus(true);
 
     dispatch(login({ email, password }))
       .unwrap()
       .then(() => {
-
-        setLoading(false);
+        setStatus(false);
       })
       .catch(() => {
-        setLoading(false);
+        setStatus(false);
       });
   };
 
-  if (isLoggedIn) {
+  if (JSON.parse(sessionStorage.getItem("user"))) {
     return <Navigate to="/menu" />;
   }
 
@@ -64,10 +70,10 @@ export default function Login(props) {
           <Form className="login-form">
             <div className="form-group">
               <label htmlFor="email">Email</label>
-              <Field name="email" type="text" className="form-control" />
+              <Field name="email" type="email" className="form-control" />
               <ErrorMessage
                 name="email"
-                component="div"
+                component="p"
                 className="alert alert-danger"
               />
             </div>
@@ -77,15 +83,15 @@ export default function Login(props) {
               <Field name="password" type="password" className="form-control" />
               <ErrorMessage
                 name="password"
-                component="div"
+                component="p"
                 className="alert alert-danger"
               />
             </div>
             
             <p>New to Food Delivery System? <Link to="/signup">Sign up</Link></p>
             <div className="form-group">
-              <button type="submit" className="btn btn-primary btn-block" disabled={loading} id="submit-btn">
-                {loading && (
+              <button type="submit" className="btn btn-primary btn-block" disabled={status} id="submit-btn">
+                {status && (
                   <span className="spinner-border spinner-border-sm"></span>
                 )}
                 <span>Login</span>
